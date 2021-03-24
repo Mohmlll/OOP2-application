d15eb3d9cd9207eaf0fb104a99c6e7551f7ece8a
Mohammed Malloul <mohammed.malloul@hva.nl>
2021-03-24 19:43:02 +0100

    begin aan binary

---
+++ OOP2-Practicum-StartProject/src/practicumopdracht/data/ObjectModelDAO.java
@@ -0,0 +1,13 @@
+package practicumopdracht.data;
+
+public class ObjectModelDAO extends ModelDAO{
+    @Override
+    public boolean save() {
+        return false;
+    }
+
+    @Override
+    public boolean load() {
+        return false;
+    }
+}
